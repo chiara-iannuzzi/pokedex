bfcd551c1a5fbeafa5f5ef976257508609efb722
Chiara <chiara@novaway.fr>
2021-10-18 18:21:06 +0200

    pokemon lists

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,30 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <section>
+    <ul class="lists">
+        <li v-for="(pokemon, index) in pokemons" :key="index">{{ pokemon.name }}<img :src="'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/' + pokemon.id +'.png'"><a :href="'pokemon/' + pokemon.id">Voir le pokemon</a></li>
+      </ul>
+    </section>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
+import axios from 'axios';
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+    data: () => ({
+        pokemons: [],
+    }),
+    created() {
+        axios.get(`https://pokeapi.co/api/v2/pokemon?offset=0&limit=2000`)
+        .then((result) => {
+            this.pokemons = result.data.results;
+            console.log(result.data.results);
+            result.data.results.forEach(pokemon => {
+              pokemon.id = pokemon.url.split('/')
+                .filter(function(part) { return !!part }).pop();
+              this.pokemons.push(pokemon);
+            });
+        })
+    }
 }
 </script>
