1c2bd9e115b7268cc3f07b5499ede79edbba6518
Chiara <chiara@novaway.fr>
2021-10-18 18:21:19 +0200

    pokemon detail

--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,11 @@ const routes = [
     name: 'Home',
     component: Home
   },
+  {
+    path: '/pokemon/:id',
+    name: 'Detail',
+    component: () => import(/* webpackChunkName: "about" */ '../views/PokemonDetail.vue')
+  },
 ]
 
 const router = createRouter({
---
+++ src/views/PokemonDetail.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="detail">
+    <h1>{{ $route.params.id }}</h1>
+    <p>{{ pokemon.species.name }}</p>
+  </div>
+</template>
+
+<script>
+
+import axios from 'axios';
+
+export default {
+    data: () => ({
+        pokemon: null
+    }),
+    created() {
+        axios.get(`https://pokeapi.co/api/v2/pokemon/${this.$route.params.id}`)
+        .then((result) => {
+            this.pokemon = result.data;
+        })
+    }
+}
+</script>
