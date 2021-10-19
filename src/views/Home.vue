223fd53032aa04b7c011ec2ecbe18ae8aeb614a9
Chiara <chiara@novaway.fr>
2021-10-19 13:36:24 +0200

    add styles for lists

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,15 @@
 <template>
-  <section>
-    <ul class="lists">
-        <li v-for="(pokemon, index) in pokemons" :key="index">{{ pokemon.name }}<img :src="'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/' + pokemon.id +'.png'"><a :href="'pokemon/' + pokemon.id">Voir le pokemon</a></li>
-      </ul>
+  <section class="row">
+    <div class="pokemon-list">
+        <a :href="'pokemon/' + pokemon.id" class="pokemon-list-item" v-for="(pokemon, index) in pokemons" :key="index">
+            <img class="pokemon-list-item-img" :src="'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/' + pokemon.id +'.png'">
+            {{ pokemon.name }}
+        </a>
+    </div>
     </section>
 </template>
 
 <script>
-
 import axios from 'axios';
 
 export default {
@@ -17,11 +19,10 @@ export default {
     created() {
         axios.get(`https://pokeapi.co/api/v2/pokemon?offset=0&limit=2000`)
         .then((result) => {
-            this.pokemons = result.data.results;
-            console.log(result.data.results);
+            this.pokemons = [] ;
             result.data.results.forEach(pokemon => {
-              pokemon.id = pokemon.url.split('/')
-                .filter(function(part) { return !!part }).pop();
+              pokemon.id = pokemon.url.split('/').filter(function(part) { return !!part }).pop();
+              console.log(pokemon)
               this.pokemons.push(pokemon);
             });
         })
