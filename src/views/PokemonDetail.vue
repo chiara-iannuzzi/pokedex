40d52aa2b9527f3b95e58124df86003564434e8f
Chiara <chiara@novaway.fr>
2021-10-20 14:57:35 +0200

    add detail page

--- src/views/PokemonDetail.vue
+++ src/views/PokemonDetail.vue
@@ -1,8 +1,32 @@
 <template>
-  <div class="detail">
-    <h1>{{ $route.params.id }}</h1>
-    <p>{{ pokemon.species.name }}</p>
-  </div>
+  <section class="row">
+    <div class="pokemon-wrapper">
+      <div>
+        <img class="" :src="'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/' + this.$route.params.id +'.png'">
+      </div>
+      <div class="detail">
+        <div class="detail-name">
+          <div class="detail-name-sprites">
+            <img :src="'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'+ this.$route.params.id +'.png'">
+            <img :src="'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/' + this.$route.params.id + '.png'">
+            <p>No°{{ pokemonNumber }}</p>
+          </div>
+          <h1 class="no-margin">{{ pokemon.species.name }}</h1>
+        </div>
+        <div class="detail-description">
+          <div class="type-container">
+            <p>Type :</p>
+            <p v-for="(type, index) in types" :key="index" :class="'type-item type-' + type.type.name"><img class="type-item-image" :src="getImgUrl(type.type.name)" /><span class="type-item-name">{{ type.type.name }}</span></p>
+          </div>
+          <p v-for="(stat, index) in stats" :key="index">{{ stat.stat.name }} : {{ stat.base_stat }}</p>
+        </div>
+        <div class="detail-description">
+          <p>{{ pokemonDescriptionFirst }}</p>
+          <p>{{ pokemonDescriptionSecond }}</p>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
@@ -11,12 +35,40 @@ import axios from 'axios';
 
 export default {
     data: () => ({
-        pokemon: null
+        pokemon: null,
+        stats : [],
+        types: [],
+        pokemonDescriptionFirst: null,
+        pokemonDescriptionSecond: null,
+        pokemonNumber : null,
     }),
+    methods: {
+      getImgUrl(pic) {
+        return require('../assets/icons/'+ pic +'.svg')
+      }
+    },
     created() {
         axios.get(`https://pokeapi.co/api/v2/pokemon/${this.$route.params.id}`)
         .then((result) => {
             this.pokemon = result.data;
+            //Get stats of pokemon
+            this.stats = [];
+            result.data.stats.forEach(stat => {
+              this.stats.push(stat);
+            });
+            //Get types of pokemon
+            this.types = [];
+            result.data.types.forEach(type => {
+              this.types.push(type);
+            });
+            //fetch url to get new infos
+            axios.get(this.pokemon.species.url)
+            .then((data) => {
+              this.pokemonDescriptionFirst = data.data.flavor_text_entries[14].flavor_text;
+              this.pokemonDescriptionSecond = data.data.flavor_text_entries[15].flavor_text;
+              this.pokemonNumber = data.data.pokedex_numbers[0].entry_number;
+              console.log(this.pokemonNumber);
+            })
         })
     }
 }
