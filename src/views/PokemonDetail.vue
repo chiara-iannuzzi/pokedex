9994b93939c6ab8eae5aee6da97129476509154e
chiara iannuzzi <chiara.iannuzzi25@gmail.com>
2021-10-21 23:54:55 +0200

    add tab on detail page

--- src/views/PokemonDetail.vue
+++ src/views/PokemonDetail.vue
@@ -13,17 +13,41 @@
           </div>
           <h1 class="no-margin">{{ pokemon.species.name }}</h1>
         </div>
-        <div class="detail-description">
-              <div class="type-container">
-                <p>Type :</p>
-                <p v-for="(type, index) in types" :key="index" :class="'type-item type-' + type.type.name"><img class="type-item-image" :src="getImgUrl(type.type.name)" /><span class="type-item-name">{{ type.type.name }}</span></p>
-              </div>
-              <p v-for="(stat, index) in stats" :key="index">{{ stat.stat.name }} : {{ stat.base_stat }}</p>
-              <p v-for="(move, index) in pokemonMoves" :key="index">{{ move.move.name }}</p>
+        <ul class="nav nav-tabs" id="myTab" role="tablist">
+        <li class="nav-item" role="presentation">
+            <button class="nav-link active" id="home-tab" data-bs-toggle="tab" data-bs-target="#desc" type="button" role="tab" aria-controls="desc" aria-selected="true">Description</button>
+          </li>
+          <li class="nav-item" role="presentation">
+            <button class="nav-link" id="home-tab" data-bs-toggle="tab" data-bs-target="#home" type="button" role="tab" aria-controls="home" aria-selected="false">Home</button>
+          </li>
+          <li class="nav-item" role="presentation">
+            <button class="nav-link" id="profile-tab" data-bs-toggle="tab" data-bs-target="#profile" type="button" role="tab" aria-controls="profile" aria-selected="false">Profile</button>
+          </li>
+          <li class="nav-item" role="presentation">
+            <button class="nav-link" id="contact-tab" data-bs-toggle="tab" data-bs-target="#contact" type="button" role="tab" aria-controls="contact" aria-selected="false">Contact</button>
+          </li>
+        </ul>
+        <div class="detail-description tab-content" id="myTabContent">
+          <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
+            <div class="type-container">
+            <p>Type :</p>
+            <p v-for="(type, index) in types" :key="index" :class="'type-item type-' + type.type.name"><img class="type-item-image" :src="getImgUrl(type.type.name)" /><span class="type-item-name">{{ type.type.name }}</span></p>
           </div>
-        <div class="detail-description">
-          <p>{{ pokemonDescriptionFirst }}</p>
-          <p>{{ pokemonDescriptionSecond }}</p>
+          <p v-for="(stat, index) in stats" :key="index">{{ stat.stat.name }} : {{ stat.base_stat }}</p>
+          </div>
+          <div class="tab-pane fade" id="desc" role="tabpanel" aria-labelledby="profile-tab">
+            <p>{{ pokemonDescriptionFirst }}</p>
+            <p>{{ pokemonDescriptionSecond }}</p>
+          </div>
+          <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">
+            <div v-if="pokemonMoves.length">
+              <p v-for="(move, index) in pokemonMoves" :key="index">{{ move.move.name }}</p>
+            </div>
+            <div v-else>
+              <p>No moves registered, sorry ! Let's wait for an update :)</p>
+            </div>
+          </div>
+          <div class="tab-pane fade" id="contact" role="tabpanel" aria-labelledby="contact-tab">tab13</div>
         </div>
       </div>
     </div>
